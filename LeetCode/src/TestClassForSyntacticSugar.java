8d713dd01f2056182b63729016e8fe595da5a3ac
weihengl <weihengl@andrew.cmu.edu>
2018-11-19 13:13:14 -0800

    Solved sevaral problems on Leetcode

---
+++ LeetCode/src/TestClassForSyntacticSugar.java
@@ -0,0 +1,11 @@
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class TestClassForSyntacticSugar {
+	LinkedHashSet<Integer> set;
+	void test() {
+		set = new LinkedHashSet();
+		set.
+	}
+}
